32f1a17f9fbddbd5ed59326aff827058b6fa8426
madison <mdsncole97@gmail.com>
2023-08-05 19:29:39 -0500

    Display pet objects

--- js/script.js
+++ js/script.js
@@ -39,3 +39,30 @@ clover.sleep();
 baxter.play();
 
 console.log(clover, baxter);
+
+clover.isTired = 8;
+francine.isTired = 9;
+
+const allPets = [sora, clover, baxter, cleo, francine];
+
+const showPets = function (petArray) {
+    pets.innerHTML = "";
+
+    for (pet of petArray) {
+        let status = "ready to play";
+        if (pet.isTired >= 7) {
+            status = "sleeping";
+        }
+        const li = document.createElement("li");
+        li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}`;
+        pets.append(li);
+    }
+    
+};
+
+statusButton.addEventListener(
+    "click",
+    function () {
+        showPets(allPets);
+    }
+);
